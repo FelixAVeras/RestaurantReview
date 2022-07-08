1456943399765648602f7e6d118dae0a42606bf0
Felix Veras <fcarvajal44@gmail.com>
2022-07-08 17:55:26 -0400

    Creating project

---
+++ Config/connection.php
@@ -0,0 +1,12 @@
+<?php
+
+
+$connection = new mysqli('localhost', 'root', '', 'restaurantReviewDb');
+
+if ($connection === false) { 
+	die($connection->connect_error); 
+}
+
+$connection->close();
+
+?>
---
+++ home.php
@@ -0,0 +1,8 @@
+<?php include('./Config/connection.php'); ?>
+
+<?php 
+
+$saludo = 'Hola Felix';
+echo $saludo;
+
+?>
